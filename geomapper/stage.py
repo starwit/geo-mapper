7b621499843d0b4c1d2ce5b53b8b8f6f0f5df134
flonix8 <flstanek@googlemail.com>
2025-06-20 11:57:23 +0200

    Remove unnecessary variable

--- geomapper/stage.py
+++ geomapper/stage.py
@@ -11,8 +11,6 @@ from .geomapper import GeoMapper
 
 logger = logging.getLogger(__name__)
 
-PROMETHEUS_METRICS_PORT = 8000
-
 REDIS_PUBLISH_DURATION = Histogram('geo_mapper_redis_publish_duration', 'The time it takes to push a message onto the Redis stream',
                                    buckets=(0.0025, 0.005, 0.0075, 0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2, 0.25))
 FRAME_COUNTER = Counter('geo_mapper_frame_counter', 'How many frames have been consumed from the Redis input stream')
@@ -35,7 +33,7 @@ def run_stage():
 
     logger.setLevel(CONFIG.log_level.value)
 
-    logger.info(f'Starting prometheus metrics endpoint on port {PROMETHEUS_METRICS_PORT}')
+    logger.info(f'Starting prometheus metrics endpoint on port {CONFIG.prometheus_port}')
 
     start_http_server(CONFIG.prometheus_port)
 
